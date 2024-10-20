e3924ee910a518d127694ebfb3b4928e79a30dda
marita1234567 <m.bibichadze@sabauni.edu.ge>
2024-10-20 21:12:41 +0400

    homework14/2

---
+++ script.js
@@ -0,0 +1,36 @@
+let number = 8
+let sum = number * number
+if (sum > 100 ){
+    console.log( "კვადრატი მეტია ასზე")
+} else{
+    console.log ("კადარატი  ნაკლებია ასზე")
+}
+let digit = 62
+if (digit > 50 && digit < 100){
+console.log ("მეტია 50-ზე")
+}else{
+    console.log("ნაკლებია 100-ზე")
+}
+let num = -2
+
+if (num == 0) {
+    console.log("ნულის ტოლია")
+    
+} else if (num <0){
+    console.log("რიცხვი უარყოფითია")
+
+} else {
+    console.log(დადებითია)
+}
+let length =5
+let width = 7
+total= 2 * (length + width)
+if(total >50){
+    console.log("პერიმეტრი მეტია 50-ზე")
+}else{
+    console.log ("პერიმეტრი ნაკლებია 50-ზე")
+}
+
+
+
+
